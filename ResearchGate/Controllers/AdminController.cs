69902b935652cc5b7b26336abbee69ff7e802145
Marian Nader <73747488+mariannader@users.noreply.github.com>
2022-05-12 23:35:59 +0200

    project

---
+++ ResearchGate/Controllers/AdminController.cs
@@ -0,0 +1,179 @@
+﻿using ResearchGate.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ResearchGate.Controllers
+{
+    public class AdminController : Controller
+    {
+        dbreserchgateEntities db = new dbreserchgateEntities();
+        public static string currentUser = "";
+
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        
+        public ActionResult SignUp()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(tbl_user uvm, HttpPostedFileBase imgfile)
+        {
+            string path = uploadimgfile(imgfile);
+            if (path.Equals("-1"))
+            {
+                ViewBag.error = "Image could not be uploaded....";
+            }
+            else
+            {
+                tbl_user u = new tbl_user();
+                u.u_name = uvm.u_name;
+                u.u_email = uvm.u_email;
+                u.u_password = uvm.u_password;
+                u.u_image = path;
+                u.u_contact = uvm.u_contact;
+                u.u_position = uvm.u_position;
+                db.tbl_user.Add(u);
+                db.SaveChanges();
+                return RedirectToAction("login");
+
+
+            }
+
+            return View();
+        } //method......................... end.....................
+
+
+
+
+
+        public ActionResult login()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        
+        public ActionResult login(tbl_user avm)
+        {
+            tbl_user ad = db.tbl_user.Where(x => x.u_email == avm.u_email && x.u_password == avm.u_password).SingleOrDefault();
+            currentUser = ad.u_name;
+            if (ad != null)
+            {
+                Session["u_id"] = ad.u_id.ToString();
+                return RedirectToAction("Index", "Papers");
+            }
+            else
+            {
+                ViewBag.error = "Invalid username or password";
+            }
+            return View();
+        }
+
+
+        //public ActionResult profile(string currentUser)
+        //{
+        //    var tbl_paper = db.tbl_paper.Include(t => t.tbl_paper);
+        //    ViewBag.user = currentUser;
+        //    return View(tbl_paper.ToList());
+
+        //}
+
+
+
+
+        public ActionResult SearchForUser(String search)
+        {
+            List<tbl_user> LIST = db.tbl_user.ToList();
+            return View(db.tbl_user.Where(x => x.u_name.StartsWith(search) || x.u_email.StartsWith(search) || search == null).ToList());
+            //return View();db.tbl_user.ToList()
+        }
+        public ActionResult searchUser(String search)
+        {
+            List<tbl_user> LIST = db.tbl_user.ToList();
+            return View(db.tbl_user.Where(x => x.u_name.StartsWith(search) || x.u_email.StartsWith(search) || search == null).ToList());
+            //return View();db.tbl_user.ToList()
+        }
+
+        public ActionResult EditProfile()
+        {
+            
+
+            return View();
+        }
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
+        public string uploadimgfile(HttpPostedFileBase file)
+        {
+            Random r = new Random();
+            string path = "-1";
+            int random = r.Next();
+            if (file != null && file.ContentLength > 0)
+            {
+                string extension = Path.GetExtension(file.FileName);
+                if (extension.ToLower().Equals(".jpg") || extension.ToLower().Equals(".jpeg") || extension.ToLower().Equals(".png"))
+                {
+                    try
+                    {
+
+                        path = Path.Combine(Server.MapPath("~/Content/upload"), random + Path.GetFileName(file.FileName));
+                        file.SaveAs(path);
+                        path = "~/Content/upload/" + random + Path.GetFileName(file.FileName);
+
+                        //    ViewBag.Message = "File uploaded successfully";
+                    }
+                    catch (Exception ex)
+                    {
+                        path = "-1";
+                    }
+                }
+                else
+                {
+                    Response.Write("<script>alert('Only jpg ,jpeg or png formats are acceptable....'); </script>");
+                }
+            }
+
+            else
+            {
+                Response.Write("<script>alert('Please select a file'); </script>");
+                path = "-1";
+            }
+
+
+
+            return path;
+        }
+
+
+
+
+
+
+
+
+    }
+}
---
+++ ResearchGate/Controllers/PapersController.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ResearchGate.Models;
+
+namespace ResearchGate.Controllers
+{
+    public class PapersController : Controller
+    {
+        private dbreserchgateEntities db = new dbreserchgateEntities();
+
+        // GET: tbl_paper
+        
+        public ActionResult Index()
+        {
+            var tbl_paper = db.tbl_paper.Include(t => t.tbl_user);
+            ViewBag.user = AdminController.currentUser;
+            return View(tbl_paper.ToList());
+        }
+
+        // GET: tbl_paper/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbl_paper tbl_paper = db.tbl_paper.Find(id);
+            if (tbl_paper == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbl_paper);
+        }
+
+        // GET: tbl_paper/Create
+        public ActionResult Create()
+        {
+            ViewBag.p_fk_user = new SelectList(db.tbl_user, "u_id", "u_name");
+            return View();
+        }
+
+        // POST: tbl_paper/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(/*tbl_paper uvm, HttpPostedFileBase imgfile*/  [Bind(Include = "p_id,p_name,p_file,p_fk_user")] tbl_paper tbl_paper)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tbl_paper.Add(tbl_paper);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.p_fk_user = new SelectList(db.tbl_user, "u_id", "u_name", tbl_paper.p_fk_user);
+            return View(tbl_paper);
+
+           
+        }
+
+
+
+        public string uploadpaperfile(HttpPostedFileBase file)
+        {
+            Random r = new Random();
+            string path = "-1";
+            int random = r.Next();
+            if (file != null && file.ContentLength > 0)
+            {
+                string extension = Path.GetExtension(file.FileName);
+                if (extension.ToLower().Equals(".pdf") || extension.ToLower().Equals(".docx") )
+                {
+                    try
+                    {
+
+                        path = Path.Combine(Server.MapPath("~/Content/upload/papers"), random + Path.GetFileName(file.FileName));
+                        file.SaveAs(path);
+                        path = "~/Content/upload/papers" + random + Path.GetFileName(file.FileName);
+
+                        //    ViewBag.Message = "File uploaded successfully";
+                    }
+                    catch (Exception ex)
+                    {
+                        path = "-1";
+                    }
+                }
+                else
+                {
+                    Response.Write("<script>alert('Only pdf ,docx formats are acceptable....'); </script>");
+                }
+            }
+
+            else
+            {
+                Response.Write("<script>alert('Please select a file'); </script>");
+                path = "-1";
+            }
+
+
+
+            return path;
+        }
+
+
+
+        //private String savePaperFile(HttpPostedFileBase paperFile, String currentpaperId = "")
+        //{
+        //    if (paperFile != null)
+        //    {
+        //        var fileExtenstion = Path.GetExtension(paperFile.FileName);
+        //        var paperGuid = Guid.NewGuid().ToString();
+        //        String paperId = paperGuid + fileExtenstion; //save new image file
+        //        var filePath = Server.MapPath($"~/Uploads/papers/Users/{paperId}");
+        //        paperFile.SaveAs(filePath); //delete old image file
+        //        if (!String.IsNullOrEmpty(currentpaperId))
+        //        {
+        //            String oldImageFilePath = Server.MapPath($"~/Uploads/papers/Users/{currentpaperId}");
+        //            System.IO.File.Delete(oldImageFilePath);
+        //        }
+        //        return paperId;
+        //    }
+        //    return currentpaperId;
+        //}
+
+
+
+
+        // GET: tbl_paper/Edit/5
+
+
+
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbl_paper tbl_paper = db.tbl_paper.Find(id);
+            if (tbl_paper == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.p_fk_user = new SelectList(db.tbl_user, "u_id", "u_name", tbl_paper.p_fk_user);
+            return View(tbl_paper);
+        }
+
+        // POST: tbl_paper/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "p_id,p_name,p_file,p_fk_user")] tbl_paper tbl_paper)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tbl_paper).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.p_fk_user = new SelectList(db.tbl_user, "u_id", "u_name", tbl_paper.p_fk_user);
+            return View(tbl_paper);
+        }
+
+        // GET: tbl_paper/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbl_paper tbl_paper = db.tbl_paper.Find(id);
+            if (tbl_paper == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbl_paper);
+        }
+
+        // POST: tbl_paper/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            tbl_paper tbl_paper = db.tbl_paper.Find(id);
+            db.tbl_paper.Remove(tbl_paper);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        public ActionResult SearchForPaper(String search)
+        {
+            List<tbl_paper> LIST = db.tbl_paper.ToList();
+            return View(db.tbl_paper.Where(x => x.p_name.StartsWith(search) || x.p_file.StartsWith(search) || search == null).ToList());
+            //return View();db.tbl_user.ToList()
+        }
+
+        public ActionResult searchPaper(String search)
+        {
+            List<tbl_paper> LIST = db.tbl_paper.ToList();
+            return View(db.tbl_paper.Where(x => x.p_name.StartsWith(search) || x.p_file.StartsWith(search) || search == null).ToList());
+            //return View();db.tbl_user.ToList()
+        }
+
+
+        // GET: tbl_paper/Details/5
+        public ActionResult UserDetails(int? id , string name)
+        {
+
+            var tbl_paper = db.tbl_paper.Include(t => t.tbl_user);
+            ViewBag.user = id;
+            ViewBag.username = name;
+            return View(tbl_paper.ToList());
+        }
+
+
+    }
+}
